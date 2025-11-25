1cd6052391fedd7934d329b600571e3dd60b2b56
Marco Trevisiol <marco@trevisiol.casa>
2025-11-25 15:56:06 +0100

    Added full generation of skies

--- lib/planet_x.ex
+++ lib/planet_x.ex
@@ -1,3 +1,16 @@
+defmodule PlanetX.Utils do
+  def pairs(enum), do: enum |> Enum.to_list |> p
+  def p([]), do: []
+
+  def p([head|tail]) do
+    pair(head, tail)
+    |> Enum.concat(p(tail))
+  end
+
+  defp pair(left, rights), do: Enum.map(rights, fn x -> [left, x] end)
+end
+
+
 defmodule PlanetX do
   @moduledoc """
   Documentation for `PlanetX`.
@@ -9,6 +22,7 @@ defmodule PlanetX do
   
   @impl true
   def all_configurations() do
+    generate()
   end
 
   @impl true
@@ -47,4 +61,72 @@ defmodule PlanetX do
 
     {:ok, sky}
   end
+
+  # list all configurations
+  alias PlanetX.Utils
+  @comet_pairs [2,3,5,7,11,13,17] |> Utils.pairs
+  @dwarf_sectors @sectors
+  @dwarf_other 1..4 |> Utils.pairs
+  @x_offsets (6+1)..(18-2)
+
+  defp generate() do
+    [%{}]
+    |> Stream.flat_map(&add_dwarf_sector/1)
+    |> Stream.flat_map(&add_dwarf_other/1)
+    |> Stream.flat_map(&add_x/1)
+    |> Stream.flat_map(&add_comets/1)
+    |> Stream.flat_map(&add_asteroid_pair/1)
+    |> Stream.flat_map(&add_asteroid_pair/1)
+    |> Stream.flat_map(&add_gas/1)
+    |> Stream.flat_map(&add_gas/1)
+  end
+
+  defp add_comets(sky) do
+    @comet_pairs
+    |> Stream.reject(fn [a,b] -> Map.has_key?(sky, a) or Map.has_key?(sky, b) end)
+    |> Stream.map(fn [a,b] -> sky |> Map.put(a, "C") |> Map.put(b, "C") end)
+  end
+
+  defp add_dwarf_sector(sky) do
+    @dwarf_sectors
+    |> Stream.reject(fn s -> Map.has_key?(sky, s) or Map.has_key?(sky, add(s, 5)) end)
+    |> Stream.map(fn s -> sky |> Map.put(s, "D") |> Map.put(add(s, 5), "D") |> Map.put(:d, s) end)
+  end
+
+  defp add_dwarf_other(sky = %{d: d}) do
+    @dwarf_other
+    |> Stream.reject(fn [a,b] -> Map.has_key?(sky, add(d, a)) or Map.has_key?(sky, add(d, b)) end)
+    |> Stream.map(fn [a,b] -> sky |> Map.put(add(d, a), "D") |> Map.put(add(d, b), "D") end)
+  end
+
+  defp add_x(sky = %{d: d}) do
+    @x_offsets
+    |> Stream.reject(fn s -> Map.has_key?(sky, add(d, s)) end)
+    |> Stream.map(fn s -> sky |> Map.put(add(d, s), "X") end)
+  end
+
+  @inexisting -18
+  defp add_asteroid_pair(sky) do
+    existing_asteroid = Map.get(sky, :a, @inexisting)
+    @sectors
+    |> Stream.reject(fn s -> s <= existing_asteroid end)
+    |> Stream.reject(fn s -> Map.has_key?(sky, s) or Map.has_key?(sky, add(s,1)) end)
+    |> Stream.map(fn s -> sky |> Map.put(s, "A") |> Map.put(add(s,1), "A") |> Map.put(:a, s) end)
+  end
+
+  defp add_gas(sky) do
+    existing_gas = Map.get(sky, :g, @inexisting)
+    @sectors
+    |> Stream.reject(fn s -> s <= existing_gas end)
+    |> Stream.reject(fn s -> Map.has_key?(sky, s) end)
+    |> Stream.reject(fn s -> Map.has_key?(sky, add(s,1)) and Map.has_key?(sky, add(s,17)) end)
+    |> Stream.map(fn s -> sky |> Map.put(s, "G") |> Map.put(:g, s) end)
+    |> Stream.reject(fn sk -> Map.has_key?(sk, add(existing_gas,1)) and Map.has_key?(sk, add(existing_gas,17)) end)
+  end
+
+  defp add(a, b) do
+     s = a+b
+     (if s > 18, do: s-18, else: s)
+  end
 end
+
