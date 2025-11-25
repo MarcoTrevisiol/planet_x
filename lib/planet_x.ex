83a2f9ced63cc2587026c6977fb6bb1e0cedbb44
Marco Trevisiol <marco@trevisiol.casa>
2025-11-25 14:50:44 +0100

    Added basics to planet x domain

--- lib/planet_x.ex
+++ lib/planet_x.ex
@@ -2,8 +2,11 @@ defmodule PlanetX do
   @moduledoc """
   Documentation for `PlanetX`.
   """
-
   @behaviour Domain
+
+  @sectors 1..18
+
+  
   @impl true
   def all_configurations() do
   end
@@ -12,7 +15,36 @@ defmodule PlanetX do
   def query_types(), do: [:scan]
 
   @impl true
-  def answer(_config, {:scan, [_type, _from, _to]}) do
-    0
+  def answer(sky, {:scan, [object, from, to]}) do
+    interval = if to >= from,
+      do: from..to,
+      else: Stream.concat(from..18, 1..to)
+
+    interval
+    |> Stream.filter(fn s -> sky |> Map.get(s, "-") == object end)
+    |> Enum.count
+  end
+
+  def answer(sky, {:target, [sector]}) do
+    object = sky |> Map.get(sector, "-")
+    if object != "X", do: object, else: "-"
+  end
+
+  @impl true
+  def serialize(sky) do
+    @sectors
+    |> Enum.map(fn i -> sky |> Map.get(i, "-") end)
+    |> Enum.join
+  end
+
+  @impl true
+  def deserialize(binary) do
+    sky = binary
+    |> String.codepoints
+    |> Enum.with_index
+    |> Enum.map(fn {c, i} -> {i, c} end)
+    |> Enum.into(%{})
+
+    {:ok, sky}
   end
 end
